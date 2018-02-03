3104bae650d0e42698b6bdd8e3327b7690afc900
Mariana Barreiros <maridepp1@hotmail.com>
2018-02-02 21:16:38 -0300

    Primeira versão do site

---
+++ README.md
@@ -0,0 +1,3 @@
+# Meu primeiro Site
+
+Primeiro projeto do site feito com html5, css, js usando o Bootstrap
---
+++ js/app.js
@@ -0,0 +1,30 @@
+// Espera o carregamento do documento
+$(document).ready(function (){
+	// cria variável com referência para o elemento com o id = 'conheca-me'
+	var $btnConhecame = $('#conheca-me');
+	// adiciona ação ao clique do botão
+	$btnConhecame.on('click', function(){
+		var conf = confirm('Você deseja continuar?');
+		if(conf){
+			alert('Muito Obrigada!')
+		}else{
+			alert(':(');
+		}
+	});
+
+	$('.scroll-animate').on('click', function(event) {
+		var $element, option, duration;
+		// evita que a ação padrão do botão seja chamada.
+		event.preventDefault();
+		// pega o elemento clicado
+		$element = $($.attr(this, 'href'));
+		// opções de animação
+		options = {
+			scrollTop: $element.offset().top
+		};
+		// Duração em milisegundos
+		duration = 700;
+		// add efeito de animação
+		$('html, body').animate(options, duration);
+	});
+});
