6981246f7f06cd06add354ba41e5c7a0f5ac8e17
Mariana Barreiros <maridepp1@hotmail.com>
2018-02-14 17:53:46 -0300

    retirada de função ready do java script

--- js/app.js
+++ js/app.js
@@ -1,16 +1,16 @@
-// Espera o carregamento do documento
-$(document).ready(function (){
-	// cria variável com referência para o elemento com o id = 'conheca-me'
-	var $btnConhecame = $('#conheca-me');
-	// adiciona ação ao clique do botão
-	$btnConhecame.on('click', function(){
-		var conf = confirm('Você será redirecionada(o) para o meu curriculum online? Deseja continuar?');
-		// if(conf){
-		// 	alert('=)')
-		// }else{
-		// 	alert(':(');
-		// }
-	});
+// // Espera o carregamento do documento
+// $(document).ready(function (){
+// 	// cria variável com referência para o elemento com o id = 'conheca-me'
+// 	var $btnConhecame = $('#conheca-me');
+// 	// adiciona ação ao clique do botão
+// 	$btnConhecame.on('click', function(){
+// 		var conf = confirm('Você será redirecionada(o) para o meu curriculum online? Deseja continuar?');
+// 		// if(conf){
+// 		// 	alert('=)')
+// 		// }else{
+// 		// 	alert(':(');
+// 		// }
+// 	});
 
 	$('.scroll-animate').on('click', function(event) {
 		var $element, option, duration;
@@ -27,4 +27,4 @@ $(document).ready(function (){
 		// add efeito de animação
 		$('html, body').animate(options, duration);
 	});
-});
+// });
